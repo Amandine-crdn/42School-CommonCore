06e1dd0168750593e5068e32669abd90f6b86e24
Amandine CERDAN <acerdan@e1r9p6.42.fr>
2022-04-14 10:51:40 +0200

    enjoy

--- ExamRank03/micro_paint.c
+++ ExamRank03/micro_paint.c
@@ -4,98 +4,129 @@
 
 int ft_free(char **area, int heigh)
 {
-	int i = 0;
-	while (i < heigh)
+        int i = 0;
+        while (i < heigh)
+        {
+                free(area[i]);
+                i++;
+        }
+        return (1);
+}
+
+int     ft_check(int i, int j, float x, float y, float w, float h)
+{
+    int a = (float)i;
+    int b = (float)j;
+        if (a < y || b < x || a > y + h || b > x + w)
+                return (0);
+        if (a - y < 1.00000000 || b - x < 1.00000000 || y + h - a < 1.00000000 || x + w - b < 1.00000000)
+                return (2);
+        return (1);
+}
+
+int     main(int argc, char **argv)
+{
+    FILE    *file;
+    char    **area, c, back, symb;
+    int     i, j, heigh, width, arg;
+    float   x, y, w, h;
+
+    if (argc != 2)
 	{
-		free(area[i]);
-		i++;
+		write(2, "error nb arg\n", 13);
+		return (1);
 	}
-	return (1);
-}
-
-int ft_print_error(void)
-{
-	write(2, "Error\n", 6);
-	return (1);
-}
-
-int	ft_check(int i, int j, float x, float y, float w, float h)
-{
-	if (i < y || j < x || i > y + h || j > x + w)
-		return (0);
-	if (i - y < 1 || j - x < 1 || y + h - i < 1 || x + w - j < 1)
-		return (2);
-	return (1);
-}
-
-int	main(int argc, char **argv)
-{
-	FILE 	*file;
-	char 	**area, c, back, symb;
-	int		i, j, heigh, width, arg;
-	float	x, y, w, h;
-
-	//gestion des erreurs (nb arg, open, scan, taille)
-	if (argc != 2)
-		return (write(2, "error nb arg\n", 13) && 1);
 	if (!(file = fopen(argv[1], "r")))
-		return (ft_print_error());
-	if (fscanf(file, "%d %d %c\n", &width, &heigh, &back) != 3)
-		return (write(2, "error scan\n", 11) && fclose(file) && 1);
-	if (width <= 0 || heigh <= 0 || width > 300 || heigh > 300)
-		return (ft_print_error() && fclose(file));
-
-	// malloc + attribution back
-	area = malloc(sizeof(char *) * heigh + 1);
-	if (!area)
-		return (fclose(file) && 1);
-	i = 0;
-	while (i < heigh)
 	{
-		area[i] = malloc(sizeof(char) * width + 1);
+		write(2, "error open\n", 11);
+		return (1);
+	}
+	if (fscanf(file, "%d %d %c\n", &width, &heigh, &back) != 3)
+	{
+		write(2, "error scan 1er ligne\n",21);
+		fclose(file);
+		return (1);
+	}
+	if (heigh <= 0 || heigh > 300 || width <= 0 || width > 300)
+	{
+		write(2, "error dimension\n", 11);
+		fclose(file);
+		return (1);
+	}
+	
+    //malloc
+	area = malloc(sizeof(char *) * width + 1);
+	if (!area)
+	{
+		write(2, "error malloc\n", 12);
+		ft_free(area, width);
+		fclose(file);
+		return (1);
+	}
+
+    //background
+	i = 0;
+	while (i < width)
+	{
+		area[i] = malloc(sizeof(char) * heigh + 1);
 		if (!area)
-			return (fclose(file) && ft_free(area, i));
+		{
+			write(2, "error malloc\n", 12);
+			ft_free(area, i);
+			fclose(file);
+			return (1);
+		}
 		j = 0;
-		while (j < width)
+		while (j < heigh)
 		{
 			area[i][j] = back;
 			j++;
 		}
 		i++;
 	}
-	
-	//gestion erreur
-	while ((arg = fscanf(file, "%c %f %f %f %f %c\n", &c, &x, &y, &w, &h, &symb)) == 6)
+
+    //gestion erreur
+    while ((arg = fscanf(file, "%c %f %f %f %f %c\n", &c, &x, &y, &w, &h, &symb)) == 6)
+    {
+        if ((c != 'r' && c != 'R') || (w <= 0.00000000 || h <= 0.00000000))
+        {
+            write(2, "Ecrire r && R\n", 14);
+            ft_free(area, width);
+            fclose(file);
+            return (1);
+        }
+        //check
+        i = 0;
+        while (i < width)
+        {
+            j = 0;
+            while (j < heigh)
+            {
+                if (ft_check(i, j, x, y, w, h) && c == 'R')
+                    area[i][j] = symb;
+                if (ft_check(i, j, x, y, w, h) == 2&& c == 'r')
+                    area[i][j] = symb;
+                j++;
+            }
+            i++;
+        }
+    }
+    printf("arg = %d\n", arg);
+    //valeur de sortie de arg
+    if (arg > 0 && arg != 6)
 	{
-		if ((c != 'r' && c != 'R') || (w <= 0 || h <= 0))
-			return (ft_print_error() && ft_free(area, heigh) && fclose(file));
-		//check
-		i = 0;
-		while (i < heigh)
-		{
-			j = 0;
-			while (j < width)
-			{
-				if (ft_check(i, j, x, y, w, h) && c == 'R')
-					area[i][j] = symb;
-				if (ft_check(i, j, x, y, w, h) && c == 'r')
-					area[i][j] = symb;
-				j++;
-			}
-			i++;
-		}
+		write(2, "Error sortie\n", 13);
+		ft_free(area, width);
+		fclose(file);
+		return (1);
 	}
-	
-	//valeur de sortie de arg
-	if (arg > 0 && arg != 6)
-		return (ft_print_error() && ft_free(area, heigh) && fclose(file));
-	
-	//dessin
-	i = 0;
-	while (i < heigh)
+
+    //dessin
+    i = 0;
+	while (i < width)
 	{
 		j = 0;
-		while (j < width)
+		while (j < heigh)
 		{
 			write(1, &area[i][j], 1);
 			j++;
@@ -103,5 +134,7 @@ int	main(int argc, char **argv)
 		write(1, "\n", 1);
 		i++;
 	}
-	return (ft_free(area, heigh) && fclose(file) && 0);
+	ft_free(area, width);
+	fclose(file);
+	return (0);
 }
--- ExamRank03/mini_paint.c
+++ ExamRank03/mini_paint.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 
-int ft_free(char **area, int heigh)
+int	ft_free(char **area, int heigh)
 {
 	int i = 0;
 	while (i < heigh)
@@ -14,80 +14,107 @@ int ft_free(char **area, int heigh)
 	return (1);
 }
 
-int ft_print_error(void)
-{
-	write(2, "Error\n", 6);
-	return (1);
-}
-
 int	main(int argc, char **argv)
 {
-	FILE 	*file;
-	char 	**area, c, back, symb;
+	FILE	*file;
+	char	**area, c, back, symb;
 	int		i, j, heigh, width, arg;
 	float	x, y, res, radius;
-
-	//gestion des erreurs (nb arg, open, scan, taille)
+	
 	if (argc != 2)
-		return (write(2, "error nb arg\n", 13) && 1);
-	if (!(file = fopen(argv[1], "r")))
-		return (ft_print_error());
-	if (fscanf(file, "%d %d %c\n", &width, &heigh, &back) != 3)
-		return (write(2, "error scan\n", 11) && fclose(file) && 1);
-	if (width <= 0 || heigh <= 0 || width > 300 || heigh > 300)
-		return (ft_print_error() && fclose(file));
-
-	// malloc + attribution back
-	area = malloc(sizeof(char *) * heigh + 1);
-	if (!area)
-		return (fclose(file) && 1);
-	i = 0;
-	while (i < heigh)
 	{
-		area[i] = malloc(sizeof(char) * width + 1);
+		write(2, "error nb arg\n", 13);
+		return (1);
+	}
+	if (!(file = fopen(argv[1], "r")))
+	{
+		write(2, "error open\n", 11);
+		return (1);
+	}
+	if (fscanf(file, "%d %d %c\n", &width, &heigh, &back) != 3)
+	{
+		write(2, "error scan 1er ligne\n",21);
+		fclose(file);
+		return (1);
+	}
+	if (heigh <= 0 || heigh > 300 || width <= 0 || width > 300)
+	{
+		write(2, "error dimension\n", 11);
+		fclose(file);
+		return (1);
+	}
+	
+	//malloc
+	area = malloc(sizeof(char *) * width + 1);
+	if (!area)
+	{
+		write(2, "error malloc\n", 12);
+		ft_free(area, width);
+		fclose(file);
+		return (1);
+	}
+	//background
+	i = 0;
+	while (i < width)
+	{
+		area[i] = malloc(sizeof(char) * heigh + 1);
 		if (!area)
-			return (fclose(file) && ft_free(area, i));
+		{
+			write(2, "error malloc\n", 12);
+			ft_free(area, i);
+			fclose(file);
+			return (1);
+		}
 		j = 0;
-		while (j < width)
+		while (j < heigh)
 		{
 			area[i][j] = back;
 			j++;
 		}
 		i++;
 	}
-	
-	//gestion erreur
+	//check
 	while ((arg = fscanf(file, "%c %f %f %f %c\n", &c, &x, &y, &radius, &symb)) == 5)
 	{
 		if ((c != 'c' && c != 'C') || radius <= 0)
-			return (ft_print_error() && ft_free(area, heigh) && fclose(file));
-		//check
+		{
+			write(2, "Ecrire r && R\n", 14);
+			ft_free(area, width);
+			fclose(file);
+			return (1);
+		}
 		i = 0;
-		while (i < heigh)
+		while (i < width)
 		{
 			j = 0;
-			while (j < width)
+			while (j < heigh)
 			{
-				res = sqrt(powf(i - y, 2) + powf(j - x, 2)); 
+				int a = (float)i;
+				int b = (float)j;
+				res = sqrt(powf(a - y, 2) + powf(b - x, 2));
 				if (res <= radius)
-					if ((radius - res < 1 && c == 'c') || c == 'C')
+					if ((radius - res < 1  && c == 'c') || c == 'C')
 						area[i][j] = symb;
 				j++;
 			}
 			i++;
 		}
+
 	}
-	
-	//valeur de sortie de arg
+	printf("arg = %d\n", arg);
 	if (arg > 0 && arg != 5)
-		return (ft_print_error() && ft_free(area, heigh) && fclose(file));
-	
-	//dessin
+	{
+		write(2, "Error sortie\n", 13);
+		ft_free(area, width);
+		fclose(file);
+		return (1);
+	}
+
 	i = 0;
-	while (i < heigh)
+	while (i < width)
 	{
 		j = 0;
-		while (j < width)
+		while (j < heigh)
 		{
 			write(1, &area[i][j], 1);
 			j++;
@@ -95,5 +122,7 @@ int	main(int argc, char **argv)
 		write(1, "\n", 1);
 		i++;
 	}
-	return (ft_free(area, heigh) && fclose(file) && 0);
+	ft_free(area, width);
+	fclose(file);
+	return (0);
 }
