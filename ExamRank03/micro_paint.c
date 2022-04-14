7ea6a600915034bd348a5ac7c90e6f51f1965fc2
Amandine CERDAN <acerdan@e1r9p6.42.fr>
2022-04-14 13:04:02 +0200

    enjoy

--- ExamRank03/micro_paint.c
+++ ExamRank03/micro_paint.c
@@ -1,101 +1,93 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
-int ft_free(char **area, int heigh)
+void    ft_free(char **area, int heigh)
 {
-        int i = 0;
-        while (i < heigh)
-        {
-                free(area[i]);
-                i++;
-        }
-        return (1);
+    int i = 0;
+    while(i < heigh)
+    {
+        free(area[i]);
+        i++;
+    }
 }
 
-int     ft_check(int i, int j, float x, float y, float w, float h)
+int ft_check(int i, int j, float x, float y, float w, float h)
 {
     int a = (float)i;
     int b = (float)j;
-        if (a < y || b < x || a > y + h || b > x + w)
-                return (0);
-        if (a - y < 1.00000000 || b - x < 1.00000000 || y + h - a < 1.00000000 || x + w - b < 1.00000000)
-                return (2);
-        return (1);
+
+    if (a < y || b < x || a > y + h || b > x + w)
+        return (0);
+    if (a - y < 1.00000000 || b - x < 1.00000000 || y + h - a < 1.00000000 || x + w - b < 1.00000000)
+        return (2);
+    return (1);
 }
 
-int     main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     FILE    *file;
+    int     i, j, width, heigh, arg;
     char    **area, c, back, symb;
-    int     i, j, heigh, width, arg;
     float   x, y, w, h;
-
+    
+    //errors
     if (argc != 2)
-	{
-		write(2, "error nb arg\n", 13);
-		return (1);
-	}
-	if (!(file = fopen(argv[1], "r")))
-	{
-		write(2, "error open\n", 11);
-		return (1);
-	}
-	if (fscanf(file, "%d %d %c\n", &width, &heigh, &back) != 3)
-	{
-		write(2, "error scan 1er ligne\n",21);
-		fclose(file);
-		return (1);
-	}
-	if (heigh <= 0 || heigh > 300 || width <= 0 || width > 300)
-	{
-		write(2, "error dimension\n", 11);
-		fclose(file);
-		return (1);
-	}
-	
-    //malloc
-	area = malloc(sizeof(char *) * width + 1);
-	if (!area)
-	{
-		write(2, "error malloc\n", 12);
-		ft_free(area, width);
-		fclose(file);
-		return (1);
-	}
-
-    //background
-	i = 0;
-	while (i < width)
-	{
-		area[i] = malloc(sizeof(char) * heigh + 1);
-		if (!area)
-		{
-			write(2, "error malloc\n", 12);
-			ft_free(area, i);
-			fclose(file);
-			return (1);
-		}
-		j = 0;
-		while (j < heigh)
-		{
-			area[i][j] = back;
-			j++;
-		}
-		i++;
-	}
-
-    //gestion erreur
-    while ((arg = fscanf(file, "%c %f %f %f %f %c\n", &c, &x, &y, &w, &h, &symb)) == 6)
     {
-        if ((c != 'r' && c != 'R') || (w <= 0.00000000 || h <= 0.00000000))
+        write(2, "Error arg\n", 10);
+        return (1);
+    }
+    if (!(file = fopen(argv[1], "r")))
+    {
+        write(2, "Error open\n", 11);
+        return (1);
+    }
+    if (fscanf(file, "%d %d %c\n", &width, &heigh, &back) != 3)
+    {
+        write(2, "Error\n", 6);
+        fclose(file);
+        return (1);
+    }
+    if (width > 300 || width <= 0 || heigh <= 0 || heigh > 300)
+    {
+        write(2, "Error taille\n", 12);
+        fclose(file);
+        return (1);
+    }
+    //malloc
+    area = malloc(sizeof(char *) * width + 1);
+    if (!area)
+    {
+        write(2, "Error\n", 6);
+        fclose(file);
+        ft_free(area, width);
+        return (1);
+    }
+    //background
+    i = 0;
+    while (i < width)
+    {
+        area[i] = malloc(sizeof(char) * heigh + 1);
+        j = 0;
+        while (j < heigh)
         {
-            write(2, "Ecrire r && R\n", 14);
-            ft_free(area, width);
+            area[i][j] = back;
+            j++;
+        }
+        i++;
+    }
+    //exec
+    
+    while ((arg = fscanf(file, "%c %f %f %f %f %c\n", &c, &x, &y, &w, &h, &symb)) == 6) 
+    {
+        if (c != 'R' && c != 'r' && (w <= 0.00000000 || h <= 0.00000000)) 
+        {
+            write(2, "Error\n", 6);
             fclose(file);
+            ft_free(area, width);
             return (1);
         }
-        //check
+        
         i = 0;
         while (i < width)
         {
@@ -111,30 +103,28 @@ int     main(int argc, char **argv)
             i++;
         }
     }
-    printf("arg = %d\n", arg);
-    //valeur de sortie de arg
+    //check error arg
     if (arg > 0 && arg != 6)
-	{
-		write(2, "Error sortie\n", 13);
-		ft_free(area, width);
-		fclose(file);
-		return (1);
-	}
-
+    {
+        write(2, "Error\n", 6);
+        fclose(file);
+        ft_free(area, width); 
+        return (1);
+    }
     //dessin
     i = 0;
-	while (i < width)
-	{
-		j = 0;
-		while (j < heigh)
-		{
-			write(1, &area[i][j], 1);
-			j++;
-		}
-		write(1, "\n", 1);
-		i++;
-	}
-	ft_free(area, width);
-	fclose(file);
-	return (0);
+    while (i < width)
+    {
+        j = 0;
+        while (j < heigh)
+        {
+            write(1, &area[i][j], 1);
+            j++;
+        }
+        write(1, "\n", 1);
+        i++;
+    }
+    fclose(file);
+    ft_free(area, width);
+    return (0);
 }
--- ExamRank03/mini_paint.c
+++ ExamRank03/mini_paint.c
@@ -1,128 +1,126 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
 
-int	ft_free(char **area, int heigh)
+void    ft_free(char **area, int heigh)
 {
-	int i = 0;
-	while (i < heigh)
-	{
-		free(area[i]);
-		i++;
-	}
-	return (1);
+    int i = 0;
+    while(i < heigh)
+    {
+        free(area[i]);
+        i++;
+    }
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	FILE	*file;
-	char	**area, c, back, symb;
-	int		i, j, heigh, width, arg;
-	float	x, y, res, radius;
-	
-	if (argc != 2)
-	{
-		write(2, "error nb arg\n", 13);
-		return (1);
-	}
-	if (!(file = fopen(argv[1], "r")))
-	{
-		write(2, "error open\n", 11);
-		return (1);
-	}
-	if (fscanf(file, "%d %d %c\n", &width, &heigh, &back) != 3)
-	{
-		write(2, "error scan 1er ligne\n",21);
-		fclose(file);
-		return (1);
-	}
-	if (heigh <= 0 || heigh > 300 || width <= 0 || width > 300)
-	{
-		write(2, "error dimension\n", 11);
-		fclose(file);
-		return (1);
-	}
-	
-	//malloc
-	area = malloc(sizeof(char *) * width + 1);
-	if (!area)
-	{
-		write(2, "error malloc\n", 12);
-		ft_free(area, width);
-		fclose(file);
-		return (1);
-	}
-	//background
-	i = 0;
-	while (i < width)
-	{
-		area[i] = malloc(sizeof(char) * heigh + 1);
-		if (!area)
-		{
-			write(2, "error malloc\n", 12);
-			ft_free(area, i);
-			fclose(file);
-			return (1);
-		}
-		j = 0;
-		while (j < heigh)
-		{
-			area[i][j] = back;
-			j++;
-		}
-		i++;
-	}
-	//check
-	while ((arg = fscanf(file, "%c %f %f %f %c\n", &c, &x, &y, &radius, &symb)) == 5)
-	{
-		if ((c != 'c' && c != 'C') || radius <= 0)
-		{
-			write(2, "Ecrire r && R\n", 14);
-			ft_free(area, width);
-			fclose(file);
-			return (1);
-		}
-		i = 0;
-		while (i < width)
-		{
-			j = 0;
-			while (j < heigh)
-			{
-				int a = (float)i;
-				int b = (float)j;
-				res = sqrt(powf(a - y, 2) + powf(b - x, 2));
-				if (res <= radius)
-					if ((radius - res < 1  && c == 'c') || c == 'C')
-						area[i][j] = symb;
-				j++;
-			}
-			i++;
-		}
+    FILE    *file;
+    int     i, j, width, heigh, arg;
+    char    **area, symb, c, back;
+    float   x, y, radius, res;
 
-	}
-	printf("arg = %d\n", arg);
-	if (arg > 0 && arg != 5)
-	{
-		write(2, "Error sortie\n", 13);
-		ft_free(area, width);
-		fclose(file);
-		return (1);
-	}
+    if (argc != 2)
+    {
+        write(2, "error arg\n", 10);
+        return (1);
+    }
+    if (!(file = fopen(argv[1], "r")))
+    {
+        write(2, "error open\n", 11);
+        return (1);
+    }
+    if (fscanf(file, "%d %d %c\n", &width, &heigh, &back) != 3)
+    {
+        fclose(file);
+        write(2, "error scan\n", 11);
+        return (1);
+    }
+    if (width <= 0 || width > 300 || heigh <= 0 || heigh > 300)
+    {
+        fclose(file);
+        write(2, "error taille\n", 11);
+        return (1);
+    }
+    //malloc
+    area = malloc(sizeof(char *) * width + 1);
+    if (!area)
+    {
+        ft_free(area, width);
+        fclose(file);
+        write(2, "error malloc\n", 12);
+        return (1);
+    }
+    //background + malloc
+    i = 0;
+    while (i < width)
+    {
+        area[i] = malloc(sizeof(char) * heigh + 1);
+        if (!area)
+        {
+            ft_free(area, i);
+            fclose(file);
+            write(2, "error malloc\n", 12);
+            return (1);
+        }
+        j = 0;
+        while (j < heigh)
+        {
+            area[i][j] = back;
+            j++;
+        }
+        i++;
+    }
+    //exec + verif
+    while ((arg = fscanf(file, "%c %f %f %f %c\n", &c, &x, &y, &radius, &symb)) == 5) 
+    {
+        if ((c != 'c' && c != 'C') || (radius <= 0.00000000))
+        {
+            ft_free(area, i);
+            fclose(file);
+            write(2, "error coco\n", 11);
+            return (1);
+        }
+        i = 0;
+        while (i < width)
+        {
+            j = 0;
+            while (j < heigh)
+            {
+                 int a = (float)i;
+                int b = (float)j;
+                res = sqrtf(powf(a - y, 2) + powf(b - x, 2));
+                if (res <= radius)
+                    if ((radius - res < 1.00000000 && c == 'c') || c == 'C')
+                        area[i][j] = symb;
+                j++;
+            }
+            i++;
+        }
+    }
 
-	i = 0;
-	while (i < width)
-	{
-		j = 0;
-		while (j < heigh)
-		{
-			write(1, &area[i][j], 1);
-			j++;
-		}
-		write(1, "\n", 1);
-		i++;
-	}
-	ft_free(area, width);
-	fclose(file);
-	return (0);
+    //check arg sortie
+    if (arg > 0 && argc != 5)
+    {
+        ft_free(area, i);
+        fclose(file);
+        write(2, "error arg sortie\n", 16);
+        return (1);
+    }
+    //dessin
+      i = 0;
+        while (i < width)
+        {
+            j = 0;
+            while (j < heigh)
+            {
+                write(1, &area[i][j], 1);
+                j++;
+            }
+            write(1, "\n", 1);
+            i++;
+        }
+    ft_free(area, i);
+    fclose(file);
+    return (0);
 }
