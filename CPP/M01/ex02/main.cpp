ae1072676da4909bc82e05452d835470e7353856
Amandine CERDAN <acerdan@e1r1p6.clusters.42paris.fr>
2022-09-13 11:31:14 +0200

    emjoy

---
+++ CPP/M01/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+
+std::cout << "L’adresse de la string en mémoire: " << &str << std::endl;
+std::cout << "L’adresse stockée dans stringPTR: " << stringPTR << std::endl;
+std::cout << "L’adresse stockée dans stringREF: " << &stringREF << std::endl;
+
+std::cout << "La valeur de la string: " << str << std::endl;
+std::cout << "La valeur pointée par stringPTR: " << *stringPTR << std::endl;
+std::cout << "La valeur pointée par stringREF: " << stringREF << std::endl;
+}
